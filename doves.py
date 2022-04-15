d7dc8e10d119483cd0e51420d46d09d65daf1c56
Kevin Panaro <panaro.kevin@gmail.com>
2022-04-14 22:36:05 -0600

    add basic hid connect/disconnect func

--- doves.py
+++ doves.py
@@ -1,11 +1,13 @@
-
-
+import os
 import sys
+import utils
 
+from PyQt6.QtCore import Qt
 from PyQt6.QtWidgets import (QMainWindow, QPushButton,
-        QHBoxLayout, QVBoxLayout, QApplication, QToolTip, QMessageBox)
-from PyQt6.QtGui import (QFont, QAction, QIcon)
-
+        QHBoxLayout, QVBoxLayout, QApplication, QToolTip, QMessageBox,
+        QLabel, QCheckBox)
+from PyQt6.QtGui import (QFont, QAction, QIcon, QKeyEvent)
+from configparser import ConfigParser
 
 class Example(QMainWindow):
 
@@ -14,10 +16,21 @@ class Example(QMainWindow):
 
         self.initUI()
 
+        # here we initialize the hid class
+        abspath = os.path.abspath(__file__)
+        dirname = os.path.dirname(abspath)
+        os.chdir(dirname)
+        config = os.path.join(dirname, 'config.ini')
+
+        self.device = utils.QMKDevice(config)
+        
         
     def initUI(self):
         self.toolTips()
         self.quitButton()
+        self.initLabels()
+        self.initCheckbox()
+        self.initToggleButton()
 
         self.setStatusBar()
         
@@ -39,7 +52,7 @@ class Example(QMainWindow):
         btn = QPushButton("quit", self)
         btn.clicked.connect(QApplication.instance().quit)
         btn.resize(btn.sizeHint())
-        btn.move(50,50)
+        btn.move(10, 75)
 
     def closeEvent(self, event):
         """
@@ -50,6 +63,7 @@ class Example(QMainWindow):
 
         if reply == QMessageBox.StandardButton.Yes:
             event.accept()
+            self.device.close()
         else:
             event.ignore()
     
@@ -66,16 +80,54 @@ class Example(QMainWindow):
         menubar = self.menuBar()
         exitAct = self.statusBarExit()
 
-        fileMenu = menubar.addMenu('&File')
+        fileMenu = menubar.addMenu('&file')
         fileMenu.addAction(exitAct)
 
     def statusBarExit(self):
-        exitAct = QAction(QIcon("icons/close.png"), "&Exit", self)
+        exitAct = QAction(QIcon("icons/close.png"), "&exit", self)
         exitAct.setShortcut('Ctrl+Q')
         exitAct.setStatusTip('exit application')
         exitAct.triggered.connect(QApplication.instance().quit)
         return(exitAct)
 
+    def initLabels(self):
+        lbl = QLabel("QMK toolkit", self)
+        lbl.move(10, 20)
+
+    def keyPressEvent(self, a0) -> None:
+        '''
+        handle key event example
+        '''
+        if a0.key() == Qt.Key.Key_Escape.value:
+            self.close()
+
+    def initCheckbox(self):
+        cb = QCheckBox("Auto Switch", self)
+        cb.move(10, 35)
+        cb.toggle()
+        cb.stateChanged.connect(self.autoSwitchChange)
+    
+    def autoSwitchChange(self, state):
+        '''
+        eventually use to toggle features on host app
+        '''
+        if state == Qt.CheckState.Checked.value:
+            print("value checked")
+            pass
+        else:
+            pass
+
+    def initToggleButton(self):
+        hidToggle = QPushButton("hid", self)
+        hidToggle.setCheckable(True)
+        hidToggle.move(10, 55)
+        hidToggle.clicked[bool].connect(self.toggleHIDconnect)
+
+    def toggleHIDconnect(self, pressed):
+        if pressed:
+            self.device.write("hi") # this can be anything
+        else:
+            self.device.disconnect()
 
 def main():
 
